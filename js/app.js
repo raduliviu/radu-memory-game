0c639eb83a3ccd4b16e68a96d770133d634c045e
raduliviu <baltag.radu@gmail.com>
2017-11-14 15:40:35 +0100

    First commit for the project

--- js/app.js
+++ js/app.js
@@ -1,15 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
+//1. As a player, I want the board to be randomized every time I load the game so that I have a different playing experience every time
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -25,14 +14,13 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//2. As a player, if I click a card, I want it to be revealed so that I know what it represents
+//3. As a player, if I click a second card, I want it to be revealed so that I can see if it is like the first card
+//4. As a player, if I have two cards revealed that do not match, I want them to become closed again
+//5. As a player, if I have two cards revealed that match, I want them to remain open and change their color to indicate they’re matched
+//6. As a player, if I have matched all the cards on the board, I want to receive a congratulations message so that I feel good about finishing the game
+//7. As a player, I want to have a restart button on the board so that I can start the game from the beginning if I want to
+//8. As a player, I want to have a timer on the page that starts when I click the first card and ends when I finish the game, so that I know how long the game took me
+//9. As a player, I want to have a move counter on the page that increments every time I click on a card, so that I know how many moves the game took me
+//10. As a player, I want to have a star rating on the page that decreases based on the number of moves I made in the game¸
+//
