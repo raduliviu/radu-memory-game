3403101a9f24128ee354c04eb7fc12fabcf6d589
raduliviu <baltag.radu@gmail.com>
2017-11-16 17:41:00 +0100

    feat: Added timer
    I've added a timer to the page that starts when you first click on a card and ends (pauses) when you match all the cards

--- js/app.js
+++ js/app.js
@@ -58,10 +58,54 @@ $('.card').click(function() {
     //6. As a player, if I have matched all the cards on the board, I want to receive a congratulations message so that I feel good about finishing the game
     if (matchedCards === 8) {       //Placeholder, just wanted to see the mechanism works
         $('h1').css('color', 'red');
+        Clock.pause();
     }
 });
 
 //7. As a player, I want to have a restart button on the board so that I can start the game from the beginning if I want to
 //8. As a player, I want to have a timer on the page that starts when I click the first card and ends when I finish the game, so that I know how long the game took me
+//https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+var Clock = {
+    totalSeconds: 0,
+
+    start: function () {
+        var self = this;
+        function pad(val) { return val > 9 ? val : "0" + val; }
+        this.interval = setInterval(function () {
+            self.totalSeconds += 1;
+
+
+            $(".min").text(pad(Math.floor(self.totalSeconds / 60 % 60)));
+            $(".sec").text(pad(parseInt(self.totalSeconds % 60)));
+        }, 1000);
+    },
+
+    reset: function () {
+        Clock.totalSeconds = null;
+        clearInterval(this.interval);
+        $(".min").text("00");
+        $(".sec").text("00");
+    },
+
+    pause: function () {
+        clearInterval(this.interval);
+        delete this.interval;
+    },
+
+    resume: function () {
+        if (!this.interval) this.start();
+    },
+
+    restart: function () {
+        this.reset();
+        Clock.start();
+    }
+};
+
+$('.deck').one("click", function () {
+    Clock.start();
+});
+
 //9. As a player, I want to have a move counter on the page that increments every time I click on a card, so that I know how many moves the game took me
+//https://stackoverflow.com/questions/4701349/jquery-increase-the-value-of-a-counter-when-a-button-is-clicked
 //10. As a player, I want to have a star rating on the page that decreases based on the number of moves I made in the game¸
